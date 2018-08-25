cab6a0922cfe877e5c90f450a32500db42a64d2c
lucia <lucia@DESKTOP-OQ08F7E>
2018-08-25 19:05:21 -0300

    Se creó el gráfico del tablero en el paquete interfaz

--- 2048/src/interfaz/MainFrame.java
+++ 2048/src/interfaz/MainFrame.java
@@ -1,22 +1,28 @@
 package interfaz;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import java.awt.Color;
 
 public class MainFrame {
 
-	private JFrame frame;
+	private JFrame ventana;
+	private JTextField[][] cuadros;
+	private int cuadroPosX;
+	private int cuadroPosY;
 
-	/**
-	 * Launch the application.
-	 */
+	//Lanza la aplicación
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainFrame window = new MainFrame();
-					window.frame.setVisible(true);
+					MainFrame frame = new MainFrame();
+					frame.ventana.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +41,52 @@ public class MainFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana = new JFrame();
+		ventana.setTitle("Juego 2048");
+		ventana.setBounds(100, 100, 640, 480);
+		ventana.setResizable(false);
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.getContentPane().setLayout(null);
+		
+		//Título
+		JLabel titulo = new JLabel("2048");
+		titulo.setBounds(222, 11, 158, 50);
+		titulo.setHorizontalAlignment(SwingConstants.CENTER);
+		titulo.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		ventana.getContentPane().add(titulo);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		panel.setBounds(161, 69, 290, 290);
+		ventana.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		//Inicializa el tablero y la posición de los cuadros
+		cuadros=new JTextField[4][4];
+		cuadroPosX=10;
+		cuadroPosY=10;
+		
+		//Se crean los cuadros
+		for(int i=0; i<4; i++) {
+			for(int j=0; j<4; j++) {
+				
+				if(j!=0 && j<4) {
+					cuadroPosY+=70;
+				}
+				cuadros[i][j]=new JTextField();
+				cuadros[i][j].setFont(new Font("Tahoma", Font.PLAIN, 16));
+				cuadros[i][j].setHorizontalAlignment(SwingConstants.CENTER);
+				cuadros[i][j].setEditable(false);
+				cuadros[i][j].setBounds(cuadroPosX, cuadroPosY, 60, 60);
+				panel.add(cuadros[i][j]);
+				cuadros[i][j].setColumns(10);
+				
+				if(j==3) {
+					cuadroPosX+=70;
+					cuadroPosY=10;
+				}
+			}
+		}
+		
 	}
-
 }
