ec398c3d9f5352b849404f480cd3e0cb90763154
lucia <lucia@DESKTOP-OQ08F7E>
2018-08-30 10:11:27 -0300

    resuelto las sumas en todas las direcciones. Mov. casi completos

--- 2048/src/negocio/Tablero.java
+++ 2048/src/negocio/Tablero.java
@@ -135,7 +135,6 @@ public class Tablero {
 	
 			sumarDerecha();//funciona sin problemas
 			moverDerecha();//funcion casi completa
-		
 			//si logra agregar devuelve true
 			return agregarValor();
 		}
@@ -144,30 +143,27 @@ public class Tablero {
 			
 			sumarIzquierda();//funciona sin problemas
 			moverIzquierda();//funcion casi completa
-			//si logra agregar devuelve true
+			//si logra agregar, devuelve true
 			return agregarValor();
 		}
 		
 		if(direccion.equals("arriba")) {
+			sumarArriba();
 			moverArriba();
-			return true;
+			return agregarValor();
 		}
 		
 		if(direccion.equals("abajo")) {
+			sumarAbajo();
 			moverAbajo();
-			return true;
+			return agregarValor();
 		}
 		return false;
 	}
 	
 	////////////////////////////////////////////////////////////////////////////////////////
-	public boolean existeValor(int fila, int columna) {
-		return getValor(fila,columna)!=0;
-	}
-	////////////////////////////////////////////////////////////////////////////////////////
 	
 	public void sumarDerecha() {
-		System.out.println("derecha");
 		
 		//fila a fila
 		for(int i=0; i<4; i++) {
@@ -270,7 +266,6 @@ public class Tablero {
 	
 	////////////////////////////////////////////////////////////////////////////////////////
 	public void sumarIzquierda() {
-		System.out.println("derecha");
 		
 		//fila a fila
 		for(int i=0; i<4; i++) {
@@ -377,17 +372,267 @@ public class Tablero {
 		}
 	}
 	
+	public void moverIzquierda2() {
+        //fila a fila
+        for(int fila=0; fila<4; fila++) {
+            //columna a columna
+            for(int columna=3; columna>=0; columna--) {
+                
+                //me fijo si existe un valor en la columna actual
+                int actual=columna;
+                if(existeValor(fila, actual)) {
+                    //veo en la columna anterior
+                    int anterior=actual-1;
+                    //me aseguro que este dentro del rango
+                    if(anterior>=0) {
+                        //Si no existe valor
+                        if(!existeValor(fila, anterior)) {
+                            //lo muevo ahi
+                            setValor(fila,anterior,getValor(fila,actual));
+                            setValor(fila, actual, 0);
+                            //actualizo el nuevo actual y el nuevo anterior
+                            actual=anterior;
+                            anterior=anterior-1;
+                            //me aseguro que este dentro del rango
+                            if(anterior>=0) {
+                                //Si no existe valor
+                                if(!existeValor(fila, anterior)) {
+                                    //lo muevo ahi
+                                    setValor(fila,anterior,getValor(fila,actual));
+                                    setValor(fila, actual, 0);
+                                    //actualizo el nuevo actual y el nuevo anterior
+                                    actual=anterior;
+                                    anterior=anterior-1;
+                                    //me aseguro que este dentro del rango
+                                    if(anterior>=0) {
+                                        //Si no existe valor
+                                        if(!existeValor(fila, anterior)) {
+                                            //lo muevo ahi
+                                            setValor(fila,anterior,getValor(fila,actual));
+                                            setValor(fila, actual, 0);
+                                        }
+                                    }
+                                }                        
+                            }
+                        }                        
+                    }
+                }
+            }
+        }
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////	
+	public void sumarArriba() {
+		//fila a fila
+		for(int j=0; j<4; j++) {
+			//columna a columna de derecha a izquierda
+			for(int i=0; i<tamanio; i++) {
+				
+				int actual=i;
+				int siguiente=actual+1;
+				
+				if(siguiente<tamanio) {
+					//veo si hay algo en la columna actual
+					if(existeValor(actual,j)) {
+						if(existeValor(siguiente,j) && (getValor(actual,j)==getValor(siguiente,j)) ) {
+							//los sumo
+							setValor(actual,j,(getValor(actual,j))+(getValor(siguiente,j)));
+							//se quita el valor del cuadro anterior poque se sumo en el cuadro actual
+							setValor(siguiente, j, 0);
+						}
+						//Si no hay nada en el cuadro anterior
+						else if(!existeValor(siguiente,j)){
+							//me fijo en la columna siguiente
+							siguiente=siguiente+1;
+							//Me aseguro que este dentro del rango de valores posibles
+							if(siguiente<tamanio) {
+								if(existeValor(siguiente,j) 
+								&& (getValor(actual,j)==getValor(siguiente,j))) {
+									//los sumo
+									setValor(actual,j,(getValor(actual,j))+(getValor(siguiente,j)));
+									//se quita el valor del cuadro anterior poque se sumo en el cuadro actual
+									setValor(siguiente, j, 0);
+								}
+								//si no existe valor en el Siguiente anterior
+								else if(!existeValor(siguiente,j)){
+									//me fijo en la columna SigteSigteAnterior
+									siguiente=siguiente+1;
+								
+									if(siguiente<tamanio) {
+										if(existeValor(siguiente,j) 
+										&& (getValor(actual,j)==getValor(siguiente,j))) {
+											//los sumo
+											setValor(actual,j,(getValor(actual,j))+(getValor(siguiente,j)));
+											//se quita el valor del cuadro anterior poque se sumo en el cuadro actual
+											setValor(siguiente, j, 0);
+										}
+									}
+								}
+							}
+						}
+					}
+				}	
+			}
+		}
+	}
+	
 	////////////////////////////////////////////////////////////////////////////////////////
 	public void moverArriba() {
-		System.out.println("arriba");
-		//agregarValor();
+		for(int j=0; j<tamanio; j++) {
+			for(int i=0; i<tamanio; i++) {
+				
+				int actual=i;
+				int siguiente=i+1;
+				int moverAFila=0;
+				int moverAColumna=0;
+				
+				if(!existeValor(actual, j)) {
+					//guardo la posicion del cuadro libre donde voy a mover
+					moverAFila=actual;
+					moverAColumna=j;
+					
+					//me fijo si esta en rango
+					if(siguiente<tamanio) {
+						
+						if(!existeValor(siguiente, j)) {
+							
+							//actualizo el siguiente
+							siguiente=siguiente+1;
+							//verifico si esta en rango
+							if(siguiente<tamanio) {
+								//ahora veo si esta vacio
+								if(!existeValor(siguiente, j)) {
+									//actualizo el anterior
+									siguiente=siguiente+1;
+									//verifico si esta en rango
+									if(siguiente<tamanio) {
+										//ahora veo si esta vacio
+										if(existeValor(siguiente, j)) {
+											moverCuadro(siguiente,j,moverAFila,moverAColumna);
+										}
+									}
+								}
+								else {
+									moverCuadro(siguiente,j,moverAFila,moverAColumna);
+								}
+							}
+						}
+						//si existe, lo muevo al cuadro vacio que guarde
+						else {
+							moverCuadro(siguiente,j,moverAFila,moverAColumna);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////
+	public void sumarAbajo() {
+		//fila a fila
+		for(int j=0; j<4; j++) {
+			//columna a columna de derecha a izquierda
+			for(int i=3; i>0; i--) {
+				
+				int actual=i;
+				int anterior=(i-1);
+				
+				//veo si hay algo en la columna actual
+				if(existeValor(actual,j)) {
+					if(existeValor(anterior,j) && (getValor(actual,j)==getValor(anterior,j)) ) {
+						//los sumo
+						setValor(actual,j,(getValor(actual,j))+(getValor(anterior,j)));
+						//se quita el valor del cuadro anterior poque se sumo en el cuadro actual
+						setValor(anterior, j, 0);
+					}
+					//Si no hay nada en el cuadro anterior
+					else if(!existeValor(anterior,j)){
+						//me fijo en la columna siguiente Anterior
+						anterior=anterior-1;
+						//Me aseguro que este dentro del rango de valores posibles
+						if(anterior>=0) {
+							if(existeValor(anterior,j) 
+							&& (getValor(actual,j)==getValor(anterior,j))) {
+								//los sumo
+								setValor(actual,j,(getValor(actual,j))+(getValor(anterior,j)));
+								//se quita el valor del cuadro anterior poque se sumo en el cuadro actual
+								setValor(anterior, j, 0);
+							}
+							//si no existe valor en el Siguiente anterior
+							else if(!existeValor(anterior,j)){
+								//me fijo en la columna Sigte Sigte Anterior
+								anterior=anterior-1;
+								//Me aseguro que este dentro del rango de valores posibles
+								if(anterior>=0) {
+									if(existeValor(anterior,j) 
+									&& (getValor(actual,j)==getValor(anterior,j))) {
+										//los sumo
+										setValor(actual,j,(getValor(actual,j))+(getValor(anterior,j)));
+										//se quita el valor del cuadro anterior poque se sumo en el cuadro actual
+										setValor(anterior, j, 0);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 	
 	////////////////////////////////////////////////////////////////////////////////////////
 	public void moverAbajo() {
-		System.out.println("abajo");
-		//agregarValor();
+		for(int j=0; j<tamanio; j++) {
+			for(int i=tamanio-1; i>0; i--) {
+				
+				int actual=i;
+				int anterior=i-1;
+				int moverAFila=0;
+				int moverAColumna=0;
+				
+				if(!existeValor(actual, j)) {
+					//guardo la posicion del cuadro libre donde voy a mover y cuento +1
+					moverAFila=actual;
+					moverAColumna=j;
+					
+					if(!existeValor(anterior, j)) {
+						
+						//actualizo el anterior
+						anterior=anterior-1;
+						//verifico si esta en rango
+						if(anterior>=0) {
+							//ahora veo si esta vacio
+							if(!existeValor(anterior, j)) {
+								//actualizo el anterior
+								anterior=anterior-1;
+								//verifico si esta en rango
+								if(anterior>=0) {
+									//ahora veo si esta vacio
+									if(existeValor(anterior, j)) {
+										moverCuadro(anterior,j,moverAFila,moverAColumna);
+									}
+								}
+							}
+							else {
+								moverCuadro(anterior,j,moverAFila,moverAColumna);
+							}
+						}
+					}
+					//si existe, lo muevo al cuadro vacio que guarde
+					else {
+						moverCuadro(anterior,j,moverAFila,moverAColumna);
+					}
+					
+				}
+			}
+		}
 	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////
+	public boolean existeValor(int fila, int columna) {
+		return getValor(fila,columna)!=0;
+	}
+	
 
 	////////////////////////////////////////////////////////////////////////////////////////
 	public void moverCuadro(int OrigenFila,int OrigenColumna,int DestinoFila,int DestinoColumna) {
